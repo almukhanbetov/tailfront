63d6b68244874f22475f1ecda79eeb07b966aa56
almukhanbetov <almuko.m@gmail.com>
2025-09-06 10:22:20 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+ 
+  return (
+      <div className='text-7xl bg-amber-700 flex justify-center items-center'>
+          <h1 className='text-amber-50'>HELLO WORLD</h1>
+      </div>
+  )
+}
+
+export default App
